6b99497d74ef917bef579c6cc1b7742ba14c83c6
Jonathon <zimdarsj@kenyon.edu>
2023-06-28 16:56:04 -0500

    Main Page

---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles/footer.module.css';
+import Link from 'next/link';
+
+export default function Footer(){
+
+    return (
+        <div className={styles.footer}>
+          <p>Questions?</p>
+          <div>
+            <p><Link style={{color: 'black'}} href='/events'>Ceremony & Reception</Link></p>
+            <p><Link style={{color: 'black'}} href='/travel'>Travel & Stay</Link></p>
+            <p><Link style={{color: 'black'}} href='/registry'>Registry</Link></p>
+            <p><Link style={{color: 'black'}} href='rsvp'>RSVP</Link></p>
+          </div>
+          <p><a style={{color: 'black'}} href='mailto: zimdarsj@gmail.com'>Email Us</a></p>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,60 @@
 import React from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Image from 'next/image';
-import sitting from '../public/sitting.jpg';
-import circle from '../public/circlecrop.jpg';
-import styles from '../styles/ourStory.module.css';
+import bangkok from '../public/bangkoksquare.jpg';
+import villa from '../public/villasquare.jpg';
+import styles from '../styles/home.module.css';
+import Footer from '../components/Footer.jsx';
+import Link from 'next/link';
+
+const mapLink = 'https://www.google.com/maps/place/Chakrabongse+Villas+:+%E0%B8%88%E0%B8%B1%E0%B8%81%E0%B8%A3%E0%B8%9E%E0%B8%87%E0%B8%A9%E0%B9%8C+%E0%B8%A7%E0%B8%B4%E0%B8%A5%E0%B8%A5%E0%B9%88%E0%B8%B2/@13.743716,100.4886649,17z/data=!4m10!3m9!1s0x30e29904f5eb818f:0x1d98385f79476060!5m2!4m1!1i2!8m2!3d13.7437159!4d100.493171!15sChNjaGFrcmFib25nc2UgdmlsbGFzWhUiE2NoYWtyYWJvbmdzZSB2aWxsYXOSAQVob3RlbOABAA!16s%2Fg%2F1tfmf7pf?entry=tts&shorturl=1';
 
 export default function App() {
   return (
-    <div className='gray-page'>
-      <Navbar color='black' page='/' />
-      <div className='page'>
-        <h2 style={{ fontSize: '3.5rem', fontWeight: 100 }}>How it all began</h2>
-        <div>
-          <Image src={sitting} alt="sitting" style={{
-            maxWidth: '100%',
-            height: 'auto'
-          }} />
-        </div>
-        <div className={styles.quotes}>
-          <div>
-            <p>“I saw her across the bar and I couldn’t look away until I had the guts to talk to her.”</p>
-            <p>— Antoine</p>
-          </div>
-          <div>
-            <p>“We caught eye contact at one point and I was mesmerized by that addicting smile of his.”</p>
-            <p>— Soria</p>
-          </div>
-        </div>
-        <div className={styles.storyBox}>
-          <Image src={circle} alt="sitting" className={styles.storyImage} />
-          <div>
-            <p>——</p>
-            <p>Soria and Antoine met while both out with their groups of friends at a bar in Phoenix. After catching each other’s eye across the room and spending the  rest of the night finding out how much they have in common, they started dating. Two years later, we're best friends, true loves, and badass partners in life. We're finally ready to make it official! </p>
-          </div>
+    <div className={styles.greyPage}>
+      <div className={styles.date} >
+        <Navbar color='white' page='/' />
+        <div className={styles.dateText}>
+          <h2>We're Celebrating!</h2>
+          <h1>08 . 03 . 2024</h1>
+          <h2>Bangkok, Thailand</h2>
         </div>
       </div>
+      <div className={styles.events}>
+        <div>
+          <Image src={bangkok} alt="bangkok" className={styles.bangkok} />
+          <div className={styles.textBlock}>
+            <h2>Wedding</h2>
+            <p>Chakrabongse Villas</p>
+            <p>396 Maharaja Road</p>
+            <p>Tatien, Bangkok 10200</p>
+            <p className={styles.map}><a href={mapLink} target="_blank">Map</a></p>
+          </div>
+        </div>
+        <div>
+          <div className={styles.textBlock}>
+            <h2>Send Off Party</h2>
+            <p>Cheese Factory</p>
+            <p>420 Cottage Ln</p>
+            <p>Dairy, Wisconin 42069</p>
+          </div>
+          <Image src={villa} alt="villa" className={styles.villa} />
+        </div>
+      </div>
+      <div className={styles.storyBox}>
+        <hr/>
+        <div>
+          <h2>See how it all started</h2>
+          <button><Link href='/story' style={{textDecoration: 'none', color: 'white'}}>Our Story</Link></button>
+        </div>
+        <hr />
+      </div>
+      <div className={styles.rsvpBox}>
+        <p>Join Us</p>
+        <h2>We hope you can make it!</h2>
+        <button><Link href='/rsvp' style={{textDecoration: 'none', color: 'black'}}>RSVP</Link></button>
+      </div>
+      <Footer />
     </div>
   )
 }
