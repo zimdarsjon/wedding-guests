7809ed3b2c70a5e1118ae2ce31b61cf89e2535be
Jonathon <zimdarsj@kenyon.edu>
2023-07-07 11:13:35 -0500

    Finished Registry Page

--- pages/registry.js
+++ pages/registry.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar.jsx';
+import styles from '../styles/registry.module.css';
+import Footer from '../components/Footer.jsx';
 
 export default function Registry() {
   return (
     <div className='gray-page'>
-      <Navbar color='black' page='/registry'/>
-      <h1>Insert Information HERE</h1>
+      <div className={styles.imageBack}>
+        <Navbar color='white' page='/registry' />
+        <div className={styles.messageBox}>
+          <h1>Many of you are traveling across the globe to celebrate with us in Bangkok, so your presence is all we ask for.</h1>
+        </div>
+      </div>
+      <div className={styles.footerContainer}>
+        <Footer />
+      </div>
     </div>
   )
 }
